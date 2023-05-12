d21f46c90753204499d54872a76b191d30b947e0
subhadeepdas0311 <subhadeepdas0311@gmail.com>
2023-05-12 20:34:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import LayoutComponent from './components/LayoutComponent';
+
+export default function App() {
+    return (
+        <div className="App">
+            <LayoutComponent />
+        </div>
+    );
+}
